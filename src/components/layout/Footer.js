aecae40bd29504a830c12ab383146f3c8e252d0a
SeamusHarkin <36312406+aecko@users.noreply.github.com>
2022-04-18 15:24:31 +0100

    Added mobile responsiveness for footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,6 +4,8 @@ import classNames from "classnames";
 import Logo from "./partials/Logo";
 import FooterNav from "./partials/FooterNav";
 import FooterSocial from "./partials/FooterSocial";
+import Accreditations from "../sections/Accreditations";
+import { useMediaQuery } from "../../hooks/MediaHook";
 
 const propTypes = {
   topOuterDivider: PropTypes.bool,
@@ -22,28 +24,67 @@ const Footer = ({ className, topOuterDivider, topDivider, ...props }) => {
     className
   );
 
+  const isMobile = useMediaQuery();
+
   return (
-    <footer {...props} className={classes}>
-      <div className="container">
-        <div
-          className={classNames(
-            "site-footer-inner",
-            topDivider && "has-top-divider"
-          )}
-        >
-          <div className="footer-top space-between text-xxs">
-            <Logo />
-            <FooterSocial />
-          </div>
-          <div className="footer-bottom space-between text-xxs invert-order-desktop">
-            <FooterNav />
-            <div className="footer-copyright">
-              Website by <a href="https://github.com/aecko">Seamus Harkin</a>.
+    <>
+      <footer
+        {...props}
+        className={classes}
+        style={{
+          backgroundColor: "#efefef",
+          marginTop: "1em",
+        }}
+      >
+        <div className="container">
+          <div
+            className={classNames(
+              "site-footer-inner",
+              topDivider && "has-top-divider"
+            )}
+          >
+            <div className="footer-top space-between text-xxs">
+              <Logo width={350} height={70} />
+              <FooterSocial />
+            </div>
+            <div
+              className="text-xs"
+              style={{
+                display: "flex",
+                flexDirection: isMobile ? "column" : "row",
+                width: "100%",
+              }}
+            >
+              <p style={{ flex: 1 }}>
+                {
+                  "A full suite of products and services including Fire Alarm & Security Systems, Fire Suppression, Energy Monitoring & Renewable Solutions and Assistive Technologies"
+                }
+              </p>
+              <ul
+                style={{
+                  flex: 1,
+                  textAlign: isMobile ? "center" : "right",
+                  listStyle: "none",
+                }}
+              >
+                <li>📞 028 7963 9446</li>
+                <li>📧 contact@ambarsystems.com</li>
+              </ul>
+            </div>
+            <div>
+              <h4 style={{ margin: 0, marginBottom: "1em" }}>Accreditation</h4>
+              <Accreditations noMargin noDelay />
+            </div>
+            <div className="footer-bottom space-between text-xxs invert-order-desktop">
+              <FooterNav />
+              <div className="footer-copyright">
+                Website by <a href="https://github.com/aecko">Seamus Harkin</a>.
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 };
 
