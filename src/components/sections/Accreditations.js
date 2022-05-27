a36932256c36eed485cb5a5bc5cbe68938e8c88d
SeamusHarkin <36312406+aecko@users.noreply.github.com>
2022-05-27 01:19:30 +0100

    Fix Build Issue

--- src/components/sections/Accreditations.js
+++ src/components/sections/Accreditations.js
@@ -1,5 +1,4 @@
 import React from "react";
-import classNames from "classnames";
 import { SectionTilesProps } from "../../utils/SectionProps";
 import Image from "../elements/Image";
 import { ColumnList } from "./accreditationStyle";
