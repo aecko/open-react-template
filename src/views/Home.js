341a6a921d95a43f01c8f3e468e0212ccf0628f8
SeamusHarkin <36312406+aecko@users.noreply.github.com>
2022-06-15 10:59:30 +0100

    Tidy up

--- src/views/Home.js
+++ src/views/Home.js
@@ -25,29 +25,8 @@ const Home = () => {
 
   return (
     <div>
-      {/* }
-      <img
-        src={require("../assets/images/illustration-section-01.svg")}
-        style={{
-          position: "absolute",
-          top: 0,
-          right: 0,
-          zIndex: 0,
-          marginTop: "-2em",
-          opacity: "0.2",
-          width: "100%",
-          transform: "scale(1.5) translateX(5%)",
-        }}
-        alt="Design"
-      />
-      */}
       <FullScreenWrapper id="home" style={{ paddingTop: "4em" }}>
         <Hero />
-        {false && (
-          <div style={{ marginTop: isMobile ? "-1em" : "2em" }}>
-            <Accreditations noDelay />
-          </div>
-        )}
       </FullScreenWrapper>
       <OurServices className="illustration-section-03" />
       <GenericSection></GenericSection>
